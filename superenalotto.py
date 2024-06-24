8834462115e6d47b034c797d19252ac3c06e8ae1
Piso90 <127155059+Piso90@users.noreply.github.com>
2024-06-24 22:47:29 +0200

    superenalotto.py

--- superenalotto.py
+++ superenalotto.py
@@ -1,7 +1,7 @@
 import random
 
 def generate_numbers():
-    numeri = random.sample(range(1, 91), 88)
+    numeri = random.sample(range(1, 91), 6)
     numeri.sort()
     return numeri
 
@@ -13,53 +13,59 @@ def generate_jolly():
     jolly = random.randint(1, 90)
     return jolly
 
+print("")
+print("<< INIZIO PROGRAMMA >>")
+print("")
+print("")
+print("Ciao, ora ti fornisco i numeri per una giocata al superenalotto:")
+
 # Genera i 6 numeri del quadro gioco
 numeri = generate_numbers()
 superstar = generate_superstar()
 jolly = generate_jolly()
-for contsup in numeri:
-    while superstar == contsup:
-        #ricalcola superstar perchè uguale a uno dei 6 numeri
-        superstar = generate_superstar()
-for contjo in numeri:
-    while jolly == contjo:
-        #ricalcola jolly perchè uguale a uno dei 6 numeri
-        jolly = generate_jolly()
-if jolly == superstar:
-    for contjo in numeri:
-            while jolly == contjo:
-                #jolly e superstar coincidono, ricalcola
-                jolly = generate_jolly()
+while superstar in numeri:
+    #ricalcola superstar perchè uguale a uno dei 6 numeri
+    superstar = generate_superstar()
+while jolly in numeri:
+    #ricalcola jolly perchè uguale a uno dei 6 numeri
+    jolly = generate_jolly()
+tutti = numeri.copy()
+tutti.append(jolly)
+#tutti è numeri + jolly su cui verificare superstar
+while superstar in tutti:
+    #superstar già presente, ricalcola
+    superstar = generate_superstar()
 print("")
 print("Numeri quadro gioco:", numeri)
 print("Jolly:", jolly)
 print("Superstar:", superstar)
 
 # Chiedi se generare altri numeri
-risposta = input("Vuoi giocare ancora? (y/n) ")
+risposta = input("Vuoi completare un'altro quadro? (y/n) ")
 while risposta.lower() == "y":
     numeri = generate_numbers()
     superstar = generate_superstar()
     jolly = generate_jolly()
-    for contsup in numeri:
-        while superstar == contsup:
-            #ricalcola superstar perchè uguale a uno dei 6 numeri
-            superstar = generate_superstar()
-    for contjo in numeri:
-        while jolly == contjo:
-            #ricalcola jolly perchè uguale a uno dei 6 numeri
-            jolly = generate_jolly()
-    if jolly == superstar:
-        for contjo in numeri:
-            while jolly == contjo:
-                #jolly e superstar coincidono, ricalcola
-                jolly = generate_jolly()
+    while superstar in numeri:
+        #ricalcola superstar perchè uguale a uno dei 6 numeri
+        superstar = generate_superstar()
+    while jolly in numeri:
+        #ricalcola jolly perchè uguale a uno dei 6 numeri
+        jolly = generate_jolly()
+    tutti = numeri.copy()
+    tutti.append(jolly)
+    #tutti è numeri + jolly su cui verificare superstar
+    while superstar in tutti:
+        #superstar è già presente, ricalcola
+        superstar = generate_superstar()
     print("")
     print("Numeri quadro gioco:", numeri)
     print("Jolly:", jolly)
     print("Superstar:", superstar)
-    risposta = input("Vuoi giocare ancora? (y/n) ")
+    risposta = input("Vuoi completare un'altro quadro? (y/n) ")
 
 print("")
 print("Buona fortuna!")
-print("  ")
+print("")
+print("")
+print("<< FINE PROGRAMMA >>")
