844e03034c7cb7eceb912b9cb98e98a0e7301345
Samuele P. <127155059+Piso90@users.noreply.github.com>
2025-01-14 22:49:29 +0100

    superenalotto_codice.py

--- superenalotto.py
+++ superenalotto.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 def generate_numbers():
     numeri = random.sample(range(1, 91), 6)
@@ -14,10 +15,27 @@ def generate_jolly():
     return jolly
 
 print("")
+print("----LOADING_PROGRAM----")
+print("")
+time.sleep(1)
+print("----CONNECTION_ESTABLISHED----")
+print("")
+time.sleep(1)
+print("----STARTING----")
+time.sleep(1)
+print("")
+nome = input("Dimmi il tuo nome per procedere: ")
+print("")
+print("Benvenuto",nome)
+print("")
+time.sleep(1)
 print("<< INIZIO PROGRAMMA >>")
 print("")
 print("")
-print("Ciao, ora ti fornisco i numeri per una giocata al superenalotto:")
+time.sleep(1)
+print("Ciao ", nome, ", ora ti fornirò i numeri per una giocata al superenalotto:")
+time.sleep(1)
+conta = 0
 
 # Genera i 6 numeri del quadro gioco
 numeri = generate_numbers()
@@ -35,13 +53,21 @@ tutti.append(jolly)
 while superstar in tutti:
     #superstar già presente, ricalcola
     superstar = generate_superstar()
+    time.sleep(1)
 print("")
 print("Numeri quadro gioco:", numeri)
 print("Jolly:", jolly)
 print("Superstar:", superstar)
+time.sleep(1)
+conta = conta + 1
+# Conta quanti quadri forniti
 
 # Chiedi se generare altri numeri
 risposta = input("Vuoi completare un altro quadro? (y/n) ")
+time.sleep(1)
+if risposta.lower() != "y" and risposta.lower() != "n":
+    print('Premere tasto Y per confermare o tasto N per negare. Altri input errati termineranno il programma')
+    risposta = input("Vuoi completare un altro quadro? (y/n) ")
 while risposta.lower() == "y":
     numeri = generate_numbers()
     superstar = generate_superstar()
@@ -58,14 +84,27 @@ while risposta.lower() == "y":
     while superstar in tutti:
         #superstar è già presente, ricalcola
         superstar = generate_superstar()
+        time.sleep(1)
     print("")
     print("Numeri quadro gioco:", numeri)
     print("Jolly:", jolly)
     print("Superstar:", superstar)
+    conta = conta + 1
+    # Conta quanti quadri forniti
+    time.sleep(1)
     risposta = input("Vuoi completare un altro quadro? (y/n) ")
+    time.sleep(1)
+    if risposta.lower() != "y" and risposta.lower() != "n":
+        print('Premere tasto Y per confermare o tasto N per negare. Altri input errati termineranno il programma')
+        risposta = input("Vuoi completare un altro quadro? (y/n) ")
 
 print("")
-print("Buona fortuna!")
 print("")
 print("")
-print("<< FINE PROGRAMMA >>")
+if conta == 1:
+    print(nome,", ti ho fornito", conta ,"quadro di gioco. Buona fortuna!")
+else: print(nome,", ti ho fornito", conta ,"quadri di gioco. Buona fortuna!")
+time.sleep(1)
+print("")
+print("")
+print("<< TERMINE PROGRAMMA >>")
